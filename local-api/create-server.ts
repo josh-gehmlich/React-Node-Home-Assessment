e0f6d0df5b63300b588dacbb63bd14089f51979d
josh-gehmlich <joshuagehmlich29@gmail.com>
2022-06-14 10:11:34 -0400

    feat: implement route for '/list' and '/job/:jobId'

--- local-api/create-server.ts
+++ local-api/create-server.ts
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import express from "express";
+import { IJob } from './interface/list.interface';
 
 // This is the API-Service that provides the job/order data
 // Endpoints are as follows
@@ -9,13 +11,32 @@ const API_SERVICE = 'https://takehome-remote-source-api.herokuapp.com'
 
 const createServer = function () {
   const app = express()
+  let jobs = null as unknown as IJob;
+
+  (async () => {
+    try {
+      const { data } = await axios.get(API_SERVICE + "/list")
+      jobs = data;
+
+    } catch (error) {
+      throw error
+    }
+  })()
+
+
+
 
   /*
    * Candidate will finish the implementation of this endpoint
    *
    */
   app.get('/list', (req, res) => {
-    res.send('Currently Not Implemented')
+    try {
+      console.log(jobs);
+      return res.status(200).send(jobs)
+    } catch (error) {
+      res.status(500).send(error)
+    }
   })
 
   /*
---
+++ local-api/interface/list.interface.ts
@@ -0,0 +1,12 @@
+export interface IJob {
+  job_id: number;
+  title: string;
+  price: number,
+  cover_image: string
+}
+
+export interface IOrder {
+  job_id: number;
+  active: boolean;
+  completed: boolean
+}
