f17353fd1e2396cf6341c2cfa25a630b70a71e99
josh-gehmlich <joshuagehmlich29@gmail.com>
2022-06-21 10:21:35 -0400

    fix: load job every minute

--- local-api/index.ts
+++ local-api/index.ts
@@ -1,11 +1,17 @@
-import CreateServer from './create-server'
-import { loadJobs } from './jobs.data'
+import CreateServer from "./create-server";
+import { loadJobs } from "./jobs.data";
 
-const PORT = 3050
+const PORT = 3050;
 
-const ServerInstance = CreateServer()
+const ServerInstance = CreateServer();
+
+// Loads job every minute
+setInterval(async () => {
+	await loadJobs();
+}, 60000);
 
 ServerInstance.listen(PORT, async () => {
-  await loadJobs()
-  console.log(`Local API listening at http://localhost:${PORT}`)
-})
+	// load job once
+	await loadJobs();
+	console.log(`Local API listening at http://localhost:${PORT}`);
+});
